3c97794a303055420f7d6af553ee0d14450fff47
Jesse Lovelace <jesse.lovelace@gmail.com>
2007-11-08 14:47:21 +0000

    Initial import.
    git-svn-id: https://familybot.googlecode.com/svn/trunk@2 9539b4e9-6b3e-0410-9af2-078c48654188

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^familybot/', include('familybot.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)
