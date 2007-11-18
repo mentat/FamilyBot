f762424e02a66e9a6e7ba757a4458eae20bb65f9
Jesse Lovelace <jesse.lovelace@gmail.com>
2007-11-18 20:07:12 +0000

    Adding readme.
    git-svn-id: https://familybot.googlecode.com/svn/trunk@5 9539b4e9-6b3e-0410-9af2-078c48654188

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysq
 DATABASE_NAME = 'familybot'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'root'             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_HOST = '127.0.0.1'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
