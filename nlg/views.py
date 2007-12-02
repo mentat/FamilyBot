a2833043c1b2cd9648b03778f222ab0e1a89666d
Jordan O'Mara <jsomara@gmail.com>
2007-12-02 19:09:39 +0000

    changed graphviz implementation to a builder from a template
    git-svn-id: https://familybot.googlecode.com/svn/trunk@39 9539b4e9-6b3e-0410-9af2-078c48654188

--- nlg/views.py
+++ nlg/views.py
@@ -8,16 +8,62 @@ from tempfile import mkstemp
 
 def generate(request):
 	languageResponse = simplejson.loads(request["payload"])
-	response = _processTemplate(languageResponse["actions"][0]["action"],languageResponse["actions"][0])
+	response = _processTemplate(languageResponse["actions"][0]["action"],languageResponse["actions"])
 	return response
 
 def _processTemplate(templateName, response):
-	textOutput = render_to_string(templateName, {"response":response})
-	graphvizOutput = render_to_string(templateName+".dot", {"response":response})
+	"""
+	Processes a template based on our request
+	This is a two part process: first, we generate the natural language associated 
+	with the answer for the request. We use thd django templating system for this, 
+	based on the name of the relationship queried.
+	Next, we generate the graphviz output for our request.
+	"""
+	textOutput = render_to_string(templateName, {"response":response[0]})
+	graphvizOutput = _graphvizBuild({"response":response})
+	#graphvizOutput = render_to_string(templateName+".dot", {"response":response})
 	htmlTemplateData = {"stringOutput":textOutput.strip(), "graph":graphvizOutput}
 	return render_to_response("output.html", {"data":htmlTemplateData})
 	
-def generateGraphvizFile(request):
+def _graphvizBuild(response):
+	"""
+	Generates a graphviz template string for our request
+	It will fill out a graph of the entirety of the request, but color in the referenced
+	nodes a certain color based on the relationship
+	"""
+	context = response[1]
+	main = response[0]
+	contextNodes = dict([(x['id', x) for x in context["result"]])
+	mainNodes = dict([(x['id', x) for x in main["result"]])
+	graphvizString += "digraph " + context["action"] +	" {\n"
+	
+	for node in context["result"]:
+		# build the style definition for the current node
+		if main.get[node["id"]] == null:
+			graphvizString += "\t" + node["title"] + " [style=filled fillcolor=black fontcolor=white]"
+		else:
+			graphvizString += "\t" + node["title"]
+			if node["gender"] == 'M':
+				graphvizString += " [style=filled fillcolor=\"#80C9FF\" fontcolor=white]\n"
+			else: 
+				graphvizString += " [style=filled fillcolor=\"#FF80B8\" fontcolor=white]\n"
+		if node.get["mother"] != null:
+			# build the mother -> son relationship
+			graphvizString += "\t" + contextNodes[node["mother"]] + " -> " node["title"] +"\n"
+		if node.get["father"] != null:
+			# build the father -> son relationship
+			graphvizString += "\t" + contextNodes[node["father"]] + " -> " node["title"] +"\n"
+	
+	graphvizString += "\n}"
+	return graphvizString
+			
+	
+def generateGraphvizFile(response):
+	"""
+	Generates a grapvhiz template file for use in our web application response.
+	The actual parsing of the data for graphviz generation is already done at this 
+	point, so we are simply creating a temporary file to send to the web browser
+	"""
 	tmpdot, dot_filename = mkstemp()
 	tmppng, png_filename = mkstemp()
 
